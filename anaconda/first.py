f9a96d40e6e9efcb5ba5d68e732dc592dd5a737e
Sicorschi <sicorschiigor1991@gmail.com>
2021-04-03 02:54:25 +0200

    some init tools to use panda for some data

---
+++ anaconda/first.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('http://bit.ly/autompg-csv') # guardamos en la variable df (data feed) la tabla de datos.
+df.head() # visualizar los datos.
+
+%matplotlib inline
+df.plot.scatter(x='colX', y='colY')
