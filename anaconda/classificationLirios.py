287c8f622f341c07c0fff2a24968fb981ddb283a
Sicorschi <sicorschiigor1991@gmail.com>
2021-04-03 21:06:44 +0200

    added classification neural network

--- anaconda/classificationLirios.py
+++ anaconda/classificationLirios.py
@@ -104,8 +104,8 @@ models.append(('SVM', SVC()))
 results = []
 names = []
 for name, model in models:
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(
+    kfold = skmodel.KFold(n_splits=10, random_state=seed)
+    cv_results = skmodel.cross_val_score(
         model, X_train, Y_train, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
