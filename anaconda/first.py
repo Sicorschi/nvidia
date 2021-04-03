342070961994cc083840a4547bac5997d8e5f0fd
Sicorschi <sicorschiigor1991@gmail.com>
2021-04-03 18:35:30 +0200

    added new anaconda init file

--- anaconda/first.py
+++ anaconda/first.py
@@ -3,5 +3,5 @@ import pandas as pd
 df = pd.read_csv('http://bit.ly/autompg-csv') # guardamos en la variable df (data feed) la tabla de datos.
 df.head() # visualizar los datos.
 
-%matplotlib inline
+# %matplotlib inline
 df.plot.scatter(x='colX', y='colY')
